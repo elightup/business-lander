6daa9d31b058ce2cfdf623b30db69c4bd87bd400
Hương <longvt@elightup>
2019-09-07 08:57:04 +0700

    Update dashboard: thêm nút Documentation

--- inc/dashboard/sections/welcome.php
+++ inc/dashboard/sections/welcome.php
@@ -26,5 +26,6 @@
 <a target="_blank" href="<?php echo esc_url( 'https://gretathemes.com/' . $this->utm ); ?>" class="wp-badge"><?php esc_html_e( 'GretaThemes', 'business-lander' ); ?></a>
 
 <p class="about-buttons">
+	<a href="<?php echo esc_url( "https://gretathemes.com/docs/{$this->slug}/{$this->utm }" ); ?>" target="_blank" class="button button-secondary"><?php esc_html_e( 'Documentation', 'business-lander' ); ?></a>
 	<a target="_blank" class="button" href="<?php echo esc_url( 'https://gretathemes.com/support/' . $this->utm ); ?>"><?php esc_html_e( 'Support', 'business-lander' ); ?></a>
 </p>
