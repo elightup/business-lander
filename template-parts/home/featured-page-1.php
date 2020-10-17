46807f7462de654c21d255a968122abf9e943d30
Hương <lehuongshmily@gmail.com>
2020-10-17 15:35:26 +0700

    Thay đổi cách lấy các featured page theo câu truy vấn thông thường

--- template-parts/home/featured-page-1.php
+++ template-parts/home/featured-page-1.php
@@ -10,25 +10,34 @@ if ( ! $featured_page_1 ) {
 	return;
 }
 
-$char = '';
-$get_title = get_the_title( $featured_page_1 );
-if ( $get_title ) {
-	$char = $get_title[0];
+$query = new WP_Query(
+	array(
+		'post_type'      => 'page',
+		'posts_per_page' => 1,
+		'post__in'       => [$featured_page_1]
+	)
+);
+
+if ( ! $query->have_posts() ) {
+	return;
 }
 
 $image = get_the_post_thumbnail_url( $featured_page_1, 'full' );
 if ( $image ) {
 	$image = ' style="background-image: url(' . esc_url( $image ) . ')"';
 }
+
+while ( $query->have_posts() ) : $query->the_post();
 ?>
 <section class="featured-page-1" <?php echo $image; // WPCS: XSS OK. ?>>
 	<div class="container">
-		<div class="featured-page" data-line="<?php echo esc_attr( $char );?>">
+		<div class="featured-page" data-line="<?php the_title(); ?>">
 			<div class="featured-page__title">
-				<h3><?php echo esc_html( $get_title ); ?></h3>
+				<h3><?php the_title(); ?></h3>
 			</div>
-			<?php echo wp_kses_post( get_the_excerpt( $featured_page_1 ) ); ?>
-			<a href="<?php echo esc_url( get_the_permalink( $featured_page_1 ) ); ?>" class="featured-page__continue"><?php esc_html_e( 'Learn more', 'business-lander' ); ?></a>
+			<?php echo wp_kses_post( get_the_excerpt() ); ?>
+			<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="featured-page__continue"><?php esc_html_e( 'Learn more', 'business-lander' ); ?></a>
 		</div>
 	</div>
 </section>
+<?php endwhile; wp_reset_postdata(); ?>
--- template-parts/home/featured-page-3.php
+++ template-parts/home/featured-page-3.php
@@ -10,25 +10,31 @@ if ( ! $featured_page_3 ) {
 	return;
 }
 
-$char = '';
-$get_title = get_the_title( $featured_page_3 );
-if ( $get_title ) {
-	$char = $get_title[0];
+$query = new WP_Query(
+	array(
+		'post_type'      => 'page',
+		'posts_per_page' => 1,
+		'post__in'       => [$featured_page_3]
+	)
+);
+
+if ( ! $query->have_posts() ) {
+	return;
 }
-
 $thumbnail = get_the_post_thumbnail( $featured_page_3, 'full' );
+while ( $query->have_posts() ) : $query->the_post();
 ?>
-	<section class="featured-page-3">
-		<div class="container">
-			<?php echo wp_kses_post( $thumbnail ); ?>
-			<div class="featured-page" data-line="<?php echo esc_attr( $char );?>">
-				<div class="featured-page__title">
-					<h3><?php echo esc_html( $get_title ); ?></h3>
-				</div>
-				<?php echo wp_kses_post( get_the_excerpt( $featured_page_3 ) ); ?>
-				<a href="<?php echo esc_url( get_the_permalink( $featured_page_3 ) ); ?>"
-				   class="featured-page__continue"><?php esc_html_e( 'Learn more', 'business-lander' ); ?></a>
+<section class="featured-page-3">
+	<div class="container">
+		<?php echo wp_kses_post( $thumbnail ); ?>
+		<div class="featured-page" data-line="<?php echo esc_attr( $char );?>">
+			<div class="featured-page__title">
+				<h3><?php the_title(); ?></h3>
 			</div>
+			<?php echo wp_kses_post( get_the_excerpt() ); ?>
+			<a href="<?php echo esc_url( get_the_permalink() ); ?>"
+			   class="featured-page__continue"><?php esc_html_e( 'Learn more', 'business-lander' ); ?></a>
 		</div>
-	</section>
-
+	</div>
+</section>
+<?php endwhile; wp_reset_postdata(); ?>
