708816e65c45162a8e359de9ca0a57ca6d9fcac0
ToanNguyen94 <nguyenthetoan.ntt@gmail.com>
2022-12-24 09:16:49 +0700

    add list of recommended plugins

--- inc/admin/plugins.php
+++ inc/admin/plugins.php
@@ -6,6 +6,7 @@
  */
 
 add_action( 'tgmpa_register', 'business_lander_register_required_plugins' );
+add_filter( 'ocdi/register_plugins', 'business_register_plugins' );
 
 /**
  * Register required plugins
@@ -15,10 +16,10 @@ add_action( 'tgmpa_register', 'business_lander_register_required_plugins' );
 function business_lander_register_required_plugins() {
 	$plugins = business_lander_required_plugins();
 
-	$config = array(
+	$config = [
 		'id'          => 'business-lander',
-		'has_notices' => false,
-	);
+		'has_notices' => true,
+	];
 
 	tgmpa( $plugins, $config );
 }
@@ -27,22 +28,40 @@ function business_lander_register_required_plugins() {
  * List of required plugins
  */
 function business_lander_required_plugins() {
-	return array(
-		array(
+	return [
+		[
 			'name'     => esc_html__( 'Jetpack', 'business-lander' ),
 			'slug'     => 'jetpack',
-		),
-		array(
-			'name' => esc_html__( 'Ultimate Fonts', 'business-lander' ),
-			'slug' => 'ultimate-fonts',
-		),
-		array(
-			'name' => esc_html__( 'Ultimate Colors', 'business-lander' ),
-			'slug' => 'ultimate-colors',
-		),
-		array(
+		],
+		[
 			'name' => esc_html__( 'Slim SEO', 'business-lander' ),
 			'slug' => 'slim-seo',
-		),
-	);
+		],
+		[
+			'name' => esc_html__( 'Falcon', 'business-lander' ),
+			'slug' => 'falcon',
+		],
+	];
+}
+
+/**
+ * List of recommended plugins in ocdi plugin
+ */
+function business_register_plugins( $plugins ) {
+	$theme_plugins = [
+		[
+			'name'     => esc_html__( 'Jetpack', 'business-lander' ),
+			'slug'     => 'jetpack',
+		],
+		[
+			'name' => esc_html__( 'Slim SEO', 'business-lander' ),
+			'slug' => 'slim-seo',
+		],
+		[
+			'name' => esc_html__( 'Falcon', 'business-lander' ),
+			'slug' => 'falcon',
+		],
+	];
+
+	return array_merge( $plugins, $theme_plugins );
 }
