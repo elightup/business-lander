a90d26e4296ab2ac0cb0d020b5bc36e8172bdcd5
ToanNguyen94 <nguyenthetoan.ntt@gmail.com>
2022-12-29 13:44:07 +0700

    Update plugins.php

--- inc/admin/plugins.php
+++ inc/admin/plugins.php
@@ -6,22 +6,17 @@
  */
 
 add_action( 'tgmpa_register', 'business_lander_register_required_plugins' );
-add_filter( 'ocdi/register_plugins', 'business_lander_register_plugins' );
+add_filter( 'ocdi/register_plugins', 'business_lander_register_ocdi_plugins' );
 
-/**
- * Register required plugins
- *
- * @since  1.0
- */
 function business_lander_register_required_plugins() {
-	$plugins = business_lander_required_plugins();
-
-	$config = [
+	tgmpa( business_lander_required_plugins(), [
 		'id'          => 'business-lander',
 		'has_notices' => true,
-	];
+	] );
+}
 
-	tgmpa( $plugins, $config );
+function business_lander_register_ocdi_plugins( $plugins ) {
+	return array_merge( $plugins, business_lander_required_plugins() );
 }
 
 /**
@@ -30,42 +25,20 @@ function business_lander_register_required_plugins() {
 function business_lander_required_plugins() {
 	return [
 		[
-			'name'     => esc_html__( 'Jetpack', 'business-lander' ),
+			'name'     =>  'Jetpack',
 			'slug'     => 'jetpack',
 		],
 		[
-			'name' => esc_html__( 'Slim SEO', 'business-lander' ),
+			'name' =>  'Slim SEO',
 			'slug' => 'slim-seo',
 		],
 		[
-			'name' => esc_html__( 'Falcon', 'business-lander' ),
+			'name' =>  'Falcon',
 			'slug' => 'falcon',
 		],
 		[
-			'name' => esc_html__( 'One Click Demo Import', 'business-lander' ),
+			'name' =>  'One Click Demo Import',
 			'slug' => 'one-click-demo-import',
 		],
 	];
-}
-
-/**
- * List of recommended plugins in ocdi plugin
- */
-function business_lander_register_plugins( $plugins ) {
-	$theme_plugins = [
-		[
-			'name'     => esc_html__( 'Jetpack', 'business-lander' ),
-			'slug'     => 'jetpack',
-		],
-		[
-			'name' => esc_html__( 'Slim SEO', 'business-lander' ),
-			'slug' => 'slim-seo',
-		],
-		[
-			'name' => esc_html__( 'Falcon', 'business-lander' ),
-			'slug' => 'falcon',
-		],
-	];
-
-	return array_merge( $plugins, $theme_plugins );
 }
